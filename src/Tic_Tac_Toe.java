49dcb283ef6d09c262377b3396fdba43f07282d2
AtomLabX <olmpy@hotmail.com>
2022-06-07 01:10:59 -0600

    Add: commit to explain what the code is about

--- src/Tic_Tac_Toe.java
+++ src/Tic_Tac_Toe.java
@@ -1,5 +1,6 @@
 public class Tic_Tac_Toe {
     public static void main(String[] args) {
+        // The basic form of the game
         System.out.println("X O X");
         System.out.println("O X O");
         System.out.println("X X O");
